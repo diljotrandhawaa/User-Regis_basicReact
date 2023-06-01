b5d07a312f61686ae8757de96aeca0e3c37c2c0f
diljotrandhawaa <learning287@gmail.com>
2023-05-31 20:23:11 -0700

    first commit

---
+++ src/Components/UserForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Card from './UI/Card';
+import './UserForm.css';
+
+const UserForm = () => {
+    <Card className='user-form'></Card>
+};
+
+export default UserForm;
